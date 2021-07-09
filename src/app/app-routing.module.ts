624d3ba4c9e9f124fbfdb0aad171a5bffc0c2e91
Kyliann <kyliann.bourdonnay@sixense-group.com>
2021-07-09 12:07:19 +0200

    feat: start of front of files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'files', component: FilesComponent },
+  // { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
