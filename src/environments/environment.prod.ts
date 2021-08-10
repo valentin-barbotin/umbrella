14871575bb5b30db97818a4959c6f140146db461
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2021-08-11 01:33:36 +0200

    chore: add ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  api: 'http://localhost:8088/',
+  // api: 'http://localhost:8088/',
+  api: 'https://back.scrypteur.com:8443/',
   checkUploadedFiles: false
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8088/',
+  // api: 'http://localhost:8088/',
+  api: 'https://back.scrypteur.com:8443/',
   checkUploadedFiles: false
 }
 
